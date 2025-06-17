2ca8579da88e274642f1e061a2673ee94a14c3f2
brunoGermano <brunogermano3@gmail.com>
2025-06-17 11:11:33 -0300

    Acrescentado a / no arquivo de configuracao do vite para corrigir o erro do github pages

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import { pokemonsMainPagePath } from './src/globalVariables'
 export default defineConfig({
   plugins: [react()],
   // base:"/Pokemons", // COnfigura diretório de exibição do servidor, o que é mostrado no "http://localhost:5173/<base>"
-  base: pokemonsMainPagePath, // COnfigura diretório de exibição do servidor, o que é mostrado no "http://localhost:5173/<base>"
+  base: `/${pokemonsMainPagePath}`, // COnfigura diretório de exibição do servidor, o que é mostrado no "http://localhost:5173/<base>"
 })
